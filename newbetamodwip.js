f6cb4724a229eebf4e1a920e78b3c9bd8020e8fb
Nick <101673904+Nick1185@users.noreply.github.com>
2022-03-15 19:09:05 -0400

    remixermodNick1185
    Nice.
    Credit to https://github.com/JmaxdnixYT/every-sandboxels-mod/blob/main/sandboxels%20mods.js .
    Credit to https://github.com/slweeb/sandboxels/blob/main/mods/Neutronium%20Mod.js .

---
+++ newbetamodwip.js
@@ -0,0 +1,35 @@
+elements.remix_element_liquid = {
+    color: "#225253",
+    name: "Powumitch",
+    behavior: behaviors.LIQUID,
+    state: "solid",
+  	category: "remix",
+    tick: function(pixel){
+    let pixel.x = (pixel.x-(Math.cos(pixel.x-pixel.y))
+    let pixelAbove = pixelMap[pixel.x-(Math.floor(Math.random() * (2 - -1)) + -1)][pixel.y-(Math.floor(Math.random() * (2 - -1)) + -1)]
+};
+elements.remix_element_what = {
+    name: "PowdGlium",
+    color: "#2A7313",
+    behavior: [
+        "XX|CR:foam%3|XX",
+        "M2|XX|M2",
+        "M1|M1|M1"
+    ],
+    state: "solid",
+  	category: "remix",
+    tick: function(pixel){
+    let pixel.x = (pixel.x-(Math.cos(pixel.x-pixel.y))
+    let pixelAbove = pixelMap[pixel.x-(Math.floor(Math.random() * (2 - -1)) + -1)][pixel.y-(Math.floor(Math.random() * (2 - -1)) + -1)]
+    density: Math.floor(Math.random() * (20 - 1)) + 1),
+};
+elements.movinPowdonGlitch = {
+    name: "PowdonismGlitchium",
+    color: "#fa0560",
+    behavior: behaviors.POWDER,
+    category: "remix",
+    state: "solid",
+    let pixel.x = (pixel.x-(Math.cos(pixel.x-pixel.y))
+    let pixel.y = (pixel.y-(Math.tan(pixel.x-pixel.y))
+    density: 8,
+};
